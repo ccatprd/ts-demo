7560738bd23a3911593615b0446df37641a62b09
ccatprd <ccatalan@proton.me>
2025-08-27 12:41:27 -0400

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_name" {
+  description = "The name of the EKS cluster."
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint of the EKS cluster."
+  value       = module.eks.cluster_endpoint
+}
+
+output "vpc_id" {
+  description = "The ID of the created VPC."
+  value       = module.vpc.vpc_id
+}
